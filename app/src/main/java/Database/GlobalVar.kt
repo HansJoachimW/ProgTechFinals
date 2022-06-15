9ce99bc21d7d81bd641fb616613675babb0f5c29
HansJoachimW <hansjw123@gmail.com>
2022-06-15 13:59:11 +0700

    Nambah Activity

--- app/src/main/java/Database/GlobalVar.kt
+++ app/src/main/java/Database/GlobalVar.kt
@@ -5,7 +5,7 @@ import Model.Movie
 class GlobalVar {
 
     companion object{
-        val STORAGE_PERMISSION_CODE: Int=100
+        val STORAGE_PERMISSION_CODE: Int = 100
         val READ_EXTERNAL_STORAGE: Int = 100
         val listDataMovie = ArrayList<Movie>()
     }
